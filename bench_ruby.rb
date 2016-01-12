97e4244cf8592e7edcd4a3646cbc0cb2b0fed7e5
Mathieu Lecarme <mlecarme@bearstech.com>
2016-01-12 19:39:55 +0100

    Java is the default version.

--- bench_ruby.rb
+++ bench_ruby.rb
@@ -1,13 +1,49 @@
 #!/usr/bin/env ruby --profile.api
 
+require 'ipaddr'
 require 'jruby/profiler'
-
-$LOAD_PATH << './lib'
-require 'ipcat'
 
 ips = File.open('ip.txt', 'r').readlines.map(&:chomp)
 
-d = IPCat::Datacenters.new('datacenters.csv')
+class Datacenters
+  def initialize(file)
+    @starts = []
+    @ends = []
+    @urls = []
+    File.open(file, 'r').readlines.map do |line|
+      add(*line.chomp.split(','))
+    end
+  end
+
+  def add(start, stop, _name, url = nil)
+    @starts << IPAddr.new(start).to_i
+    @ends << IPAddr.new(stop).to_i
+    @urls << url
+  end
+
+  def length
+    @starts.length
+  end
+
+  def find(ipstring)
+    ip = IPAddr.new(ipstring).to_i
+    high = length - 1
+    low = 0
+    while high >= low
+      probe = ((high + low) / 2).floor.to_i
+      if @starts[probe] > ip
+        high = probe - 1
+      elsif @ends[probe] < ip
+        low = probe + 1
+      else
+        return @urls[probe]
+      end
+    end
+    nil
+  end
+end
+
+d = Datacenters.new('datacenters.csv')
 cpt = 0
 profile_data = JRuby::Profiler.profile do
   (1..10_000).each do
